78f90eafb932b7f0ff9a6f0d6c7ff9e39cc1a961
Vitor Rios <vitor.neto@br.experian.com>
2020-07-17 11:50:18 -0300

    Creating hook to  change title of document

---
+++ src/hooks/usePage/index.ts
@@ -0,0 +1,11 @@
+import { useEffect } from 'react'
+
+const DEFAULT_TITLE = 'Context-Api POC'
+
+const usePage = (title: string, overWriteTitle?: boolean) => {
+  useEffect(() =>  {
+    document.title = overWriteTitle ? title : `${title} - ${DEFAULT_TITLE}`
+  }, [title, overWriteTitle])
+}
+
+export default usePage
