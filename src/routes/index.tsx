26ae1d63e01fc6fbd27058a9905faa52a8fdfaf1
Vitor Rios <vitor.neto@br.experian.com>
2020-07-17 10:08:20 -0300

    Adding new Routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,27 +1,56 @@
 import React, { FunctionComponent } from 'react'
 import { BrowserRouter, Switch, Route } from 'react-router-dom'
 
+import { 
+  HomeOutlined,
+  PostAddOutlined,  
+} from '@material-ui/icons'
+
 import Header from '../components/Header'
 import Posts from '../screens/Posts/pages'
 import PostsDetails from '../screens/Posts/pages/details'
 import Home from '../screens/Home/pages'
 import NotFound from '../screens/NotFound/pages'
+import { makeStyles } from '@material-ui/core'
 
 export interface IRoute {
   key: string
   path: string
   component: FunctionComponent
   isMenu?: boolean
+  icon?: any
 }
 
 export const ROUTES: IRoute[] = [
-  { key: 'Home', path: '/', component: Home, isMenu: true },
-  { key: 'Posts', path: '/posts', component: Posts, isMenu: true },
-  { key: 'Posts', path: '/posts/:id', component: PostsDetails },
-  { key: 'NotFound', path: '*', component: NotFound },
+  {
+    key: 'Home',
+    path: '/',
+    component: Home,
+    isMenu: true,
+    icon: HomeOutlined,
+  },
+  { 
+    key: 'Posts',
+    path: '/posts',
+    component: Posts,
+    isMenu: true,
+    icon: PostAddOutlined,
+  },
+  { 
+    key: 'Posts',
+    path: '/posts/:id',
+    component: PostsDetails,
+  },
+  {
+    key: 'NotFound',
+    path: '*',
+    component: NotFound,
+  },
 ]
 
 const Routes = () => {
+  const classes = useStyles()
+
   const renderRoutes = (
     ROUTES.map((route) => (
       <Route
@@ -36,11 +65,19 @@ const Routes = () => {
   return (
     <BrowserRouter>
     <Header />
-      <Switch>
-        {renderRoutes}
-      </Switch>
+      <div className={classes.content}>
+        <Switch>
+          {renderRoutes}
+        </Switch>
+      </div>
     </BrowserRouter>
   )
 }
 
 export default Routes
+
+const useStyles = makeStyles({
+  content: {
+    marginTop: 56,
+  },
+})
