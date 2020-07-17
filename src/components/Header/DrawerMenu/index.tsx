c630f0d8927af1d26beeb2f79eeaf92ae0008dd2
Vitor Rios <vitor.neto@br.experian.com>
2020-07-17 10:07:46 -0300

    Creating Drawer menu

---
+++ src/components/Header/DrawerMenu/index.tsx
@@ -0,0 +1,69 @@
+import React, { useState, KeyboardEvent, MouseEvent } from 'react'
+import { Drawer, makeStyles, List, ListItem, ListItemIcon, ListItemText } from '@material-ui/core'
+import { ROUTES } from '../../../routes'
+import { Link, useHistory } from 'react-router-dom'
+
+interface Props {
+  isOpen: boolean
+  setOpen: (value: boolean) => void
+}
+
+const DrawerMenu = ({ isOpen = false, setOpen }: Props) => {
+  const classes = useStyles()
+  const history = useHistory()
+
+  const toggleDrawer = (open: boolean) => (event: KeyboardEvent | MouseEvent) => {
+    if (
+      event.type === 'keydown' &&
+      ((event as React.KeyboardEvent).key === 'Tab' ||
+        (event as React.KeyboardEvent).key === 'Shift')
+    ) {
+      return;
+    }
+
+    setOpen(open)
+  }
+
+  const goTo = (path: string) => {
+    setOpen(false)
+    history.push(path)
+  }
+
+  const menuList = (
+    <div
+      className={classes.list}
+      onClick={() => toggleDrawer(false)}
+      onKeyDown={() => toggleDrawer(false)}
+      role="presentation"
+    >
+      <List>
+        {
+          ROUTES.filter(route => route.isMenu)
+            .map(({ key, path, icon: Icon }) => (
+              <ListItem key={key} onClick={() => goTo(path)} button>
+                <ListItemIcon>
+                  <Icon />
+                </ListItemIcon>
+                <ListItemText primary={key} />
+              </ListItem>
+            )
+          )
+        }
+      </List>
+    </div>
+  )
+
+  return (
+    <Drawer anchor="left" open={isOpen} onClose={toggleDrawer(false)}>
+      {menuList}
+    </Drawer>
+  )
+}
+
+export default DrawerMenu
+
+const useStyles = makeStyles({
+  list: {
+    width: 250,
+  },
+});
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,23 +1,63 @@
-import React from 'react'
+import React, { useState, useEffect } from 'react'
 
 import { ROUTES } from '../../routes'
-import { Link } from 'react-router-dom'
+import { Link, useRouteMatch, useLocation } from 'react-router-dom'
+import { AppBar, Toolbar, IconButton, Typography, Button, makeStyles, Drawer } from '@material-ui/core'
+
+import MenuIcon from '@material-ui/icons/Menu';
+import DrawerMenu from './DrawerMenu';
 
 const Header = () => {
-  const renderMenu = (
-    ROUTES.filter(route => route.isMenu)
-      .map((menu) => (
-      <li key={menu.key}>
-        <Link to={menu.path}>{menu.key}</Link>
-      </li>
-    ))
-  )
+  const classes = useStyles()
+  const [open, setOpen] = useState(false)
+  const [activeRoute, setActiveRoute] = useState('')
+  const location = useLocation()
+
+  useEffect(() => {
+    const [routeDescription] = ROUTES.filter(item => item.path === location.pathname)
+
+    if (routeDescription) {
+      setActiveRoute(routeDescription.key)
+    }
+  }, [location])
+
+  const toggleOpenMenu = (value: boolean) => {
+    setOpen(value)
+  }
 
   return (
-    <ul>
-      {renderMenu}
-    </ul>
+    <>
+      <AppBar position="fixed">
+        <Toolbar>
+          <IconButton
+            onClick={() => toggleOpenMenu(true)}
+            edge="start"
+            className={classes.menuButton}
+            color="inherit"
+            aria-label="menu"
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" className={classes.title}>
+            {activeRoute}
+          </Typography>
+        </Toolbar>
+      </AppBar>
+      <DrawerMenu isOpen={open} setOpen={toggleOpenMenu} />
+    </>
   )
 }
 
 export default Header
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  menuButton: {
+    marginRight: theme.spacing(2),
+  },
+  title: {
+    flexGrow: 1,
+  },
+}));
