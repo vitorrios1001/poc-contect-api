b3f0b1fe924bf83e6c28fd3f6b93e41a67e870c3
Vitor Rios <vitor.neto@br.experian.com>
2020-07-17 10:07:57 -0300

    Creating Generic Loader

---
+++ src/components/GenericLoader/index.tsx
@@ -0,0 +1,36 @@
+import React, { ReactElement } from 'react'
+import { CircularProgress, Typography, makeStyles } from '@material-ui/core'
+
+interface Props {
+  loading?: boolean
+  text?: string
+}
+
+const GenericLoader = ({ loading = true, text }: Props) => {
+  const classes = useStyles()
+
+  return (
+    <div className={classes.container}>
+      <CircularProgress className={classes.loader} />
+      <Typography>
+        {text}
+      </Typography>
+    </div>
+  )
+}
+
+export default GenericLoader
+
+const useStyles = makeStyles({
+  container: {
+    display: 'flex',
+    flex: 1,
+    flexDirection: 'column',
+    alignItems: 'center',
+    justifyContent: 'center',
+    margin: '12vw 0',
+  },
+  loader: {
+    padding: '2vw',
+  }
+})
